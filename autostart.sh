7d7e321ca433832988ba4c0eb2ca4801502b85b9
Jane42070 <jql1377219787@gmail.com>
2021-01-01 12:46:21 +0800

    changed format

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 ./wallpaper.sh &
 while true; do
+	disk=df -h | awk 'NR==2{print $5}'
 	meminfo=`free | awk 'NR==2 {printf("%dMB", $3/1024)}'`
-	time=`date "+%y/%m/%d %H:%M:%S"`
-	xsetroot -name "|$meminfo|$time"
+	time=`date "+%m/%d %H:%M"`
+	xsetroot -name "$disk $meminfo $time"
 	sleep 1s
 done
